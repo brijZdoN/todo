da8cfdadf083adc937813383502db8f1f894e51a
Brij Pratap Singh <brijjadaun@gmail.com>
2020-05-15 00:58:09 +0530

    minor changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget
 {
-  @override
-  State<StatefulWidget> createState()
-   {
-    
-    return null;
+ @override
+  _HomeScreenState createState() =>_HomeScreenState() ;
+   
   }
   
+  class _HomeScreenState extends State<HomeScreen>
+   {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar:AppBar(
+        title:Text("Todo")
+      ),
+    );
+  }
 }
+
+ 
